e6654f19ed88e62f3fa41d263d2200d9e5f04144
neeleshpoli <neelesh.poli2006@gmail.com>
2020-10-09 12:10:44 -0500

    Reduced lines of js code

--- am.js
+++ am.js
@@ -6,13 +6,8 @@ var posi
 console.log(center)
 console.log(screen.width)
 var id = setInterval(frame, 2);
-function frame() {
-    if (pos == center) {
-        clearInterval(id);
-    }else{
-        pos+10;
-        posi = pos.concat(px) 
-        ele.style.left = posi;
-    }
+while (pos >= center) {
+    pos+10;
+    posi = pos + px 
+    ele.style.left = posi;
 }
-frame()

146b5bb7d1426a3d433fe3d209b2139b198b0886
neeleshpoli <neelesh.poli2006@gmail.com>
2020-10-09 14:05:52 -0500

    Fixed code

--- am.js
+++ am.js
@@ -1,12 +1,12 @@
 'use strict'
 var ele = document.getElementById("we")   
 var pos
-var center = window.width/2
+var center = (parseFloat(window.width))/2
 var posi
 console.log(center)
 console.log(window.width)
 
-while (pos >= center) {
+while (pos <= center) {
     pos+10
     posi = pos + px 
     ele.style.left = posi

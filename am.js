824658e0a7ca7d325112b37cc1a7fddbea587823
neeleshpoli <neelesh.poli2006@gmail.com>
2020-10-09 11:47:56 -0500

    Fixed a little js

--- am.js
+++ am.js
@@ -2,13 +2,17 @@
 var ele = document.getElementById("wel")   
 var pos = 0;
 var center = screen.width/2
+var posi
+console.log(center)
+console.log(screen.width)
 var id = setInterval(frame, 2);
 function frame() {
     if (pos == center) {
         clearInterval(id);
     }else{
-        pos+10; 
-        ele.style.left += pos;
+        pos+10;
+        posi = pos.concat(px) 
+        ele.style.left = posi;
     }
 }
 frame()

20d3f81cf82c281c3e89b22efe80a0800008cb25
neeleshpoli <neelesh.poli2006@gmail.com>
2020-10-09 09:02:21 -0500

    Forgot to add something do...

--- am.js
+++ am.js
@@ -1,13 +1,14 @@
 'use strict'
 var ele = document.getElementById("wel")   
 var pos = 0;
-var id = setInterval(frame, 2);
 var center = screen.width/2
+var id = setInterval(frame, 2);
 function frame() {
-  if (pos == centerpx) {
-    clearInterval(id);
-  } else {
-    pos++; 
-    ele.style.left = pos + 'px'; 
+    if (pos == center) {
+        clearInterval(id);
+    }else{
+        pos++; 
+        ele.style.left = pos + 'px'; 
     }
 }
+frame()

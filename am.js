79fcf13d526410d8c517b2f15d4e22eb0ff62060
neeleshpoli <neelesh.poli2006@gmail.com>
2020-10-09 08:35:14 -0500

    fixed a error

--- am.js
+++ am.js
@@ -1,1 +1,11 @@
-var ele = document.getElementById("wel")
+var ele = document.getElementById("wel")   
+var pos = 0;
+var id = setInterval(frame, 10);
+function frame() {
+  if (pos == center) {
+    clearInterval(id);
+  } else {
+    pos++; 
+    ele.style.left = pos + 'px'; 
+    }
+}

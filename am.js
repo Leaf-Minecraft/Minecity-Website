ce35d4a1d4825288443471237031376384076905
neeleshpoli <neelesh.poli2006@gmail.com>
2020-10-09 12:20:47 -0500

    Fixed js(doesn't work)

--- am.js
+++ am.js
@@ -5,9 +5,10 @@ var center = screen.width/2
 var posi
 console.log(center)
 console.log(screen.width)
-var id = setInterval(frame, 2);
+
 while (pos >= center) {
     pos+10;
     posi = pos + px 
     ele.style.left = posi;
+    await(0.01)
 }
